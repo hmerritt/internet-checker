0ae475b7bc931ab7508cc369a807a8ab36e62d2c
Harry Merritt <harry@merritt.es>
2020-03-19 01:39:57 +0000

    Create basic scss styles

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import "./styles/index.scss";
 
 function App() {
 
     return (
         <div className="App">
-            Hello World
+            <h1>Hello World</h1>
         </div>
     );
 }
