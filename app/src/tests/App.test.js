8b4fb5f6921dc8748da95cab61108744f1ea45b6
Harry Merritt <harry@merritt.es>
2020-03-31 15:50:23 +0100

    Add basic react tests

--- app/src/tests/App.test.js
+++ app/src/tests/App.test.js
@@ -2,7 +2,28 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
-    const { getByText } = render(<App />);
-    getByText(/Hello World/i);
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reducer } from "../reducers";
+
+//  Create redux store
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
+test("renders app", () => {
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+});
+
+test("renders checking on load", () => {
+    const { getAllByText } = render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+    getAllByText(/checking/i);
 });
