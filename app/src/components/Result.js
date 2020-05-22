8b0d4b7fc04b136697ee160ce4b3cc709978f382
Harry Merritt <harry@merritt.es>
2020-05-22 19:36:13 +0100

    Improve accuracy of result check time * exact relative time of check -> 8 seconds ago

--- app/src/components/Result.js
+++ app/src/components/Result.js
@@ -2,6 +2,13 @@ import React from "react";
 import moment from "moment";
 
 function Result({ result, id }) {
+    moment.updateLocale('en', {
+        relativeTime : {
+            s  : '%d seconds',
+            ss : '%d seconds',
+        }
+    });
+
     return (
         <div className="result" status={result.status}>
             <div>
@@ -10,7 +17,7 @@ function Result({ result, id }) {
             <div>{id}</div>
             <div>{result.status}</div>
             <div>{result.ping}ms</div>
-            <div>{moment(result.request_start).calendar()}</div>
+            <div>{moment(result.request_start).fromNow()}</div>
         </div>
     );
 }
